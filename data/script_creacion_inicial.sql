ebb6df7b6944b817f276feb53a41c36448d3515e
GonzaV <vieygonzalo@gmail.com>
2018-05-23 23:51:23 -0300

    Agrego tablas Usuario, Rol por Usuario y Rolj

--- data/script_creacion_inicial.sql
+++ data/script_creacion_inicial.sql
@@ -15,3 +15,29 @@ GO
 CREATE SCHEMA TRAEME_LA_COPA_MESSI AUTHORIZATION gdHotel2018
 
 GO
+
+CREATE TABLE TRAEME_LA_COPA_MESSI.Usuario(
+Username int IDENTITY(1,1) PRIMARY KEY,
+Pass nvarchar(255)  NOT NULL,
+Nombre nvarchar(255) NOT NULL,
+Apellido nvarchar(255) NOT NULL,
+TipoDoc nvarchar(255) NOT NULL,
+NroDocumento int NOT NULL,
+Email nvarchar(255) UNIQUE,
+Telefono int NULL,
+FechaNacimiento datetime NULL,
+LogsFallidos int NOT NULL
+);
+
+CREATE TABLE TRAEME_LA_COPA_MESSI.Rol(
+IdRol int IDENTITY(1,1) PRIMARY KEY,
+Nombre nvarchar(255) NOT NULL,
+Estado int NOT NULL --No deberia ser int, luego lo cambio
+);
+
+CREATE TABLE TRAEME_LA_COPA_MESSI.RolPorUsuario(
+Username int FOREIGN KEY REFERENCES TRAEME_LA_COPA_MESSI.Usuario(Username),
+IdRol int FOREIGN KEY REFERENCES TRAEME_LA_COPA_MESSI.Rol(IdRol),
+CONSTRAINT IdRolPorUsuario PRIMARY KEY(Username,IdRol)
+);
+
