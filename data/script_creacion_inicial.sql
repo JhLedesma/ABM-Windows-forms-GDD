86613ab75e717e88f7746da430471aee2f228941
GonzaV <vieygonzalo@gmail.com>
2018-05-24 00:48:08 -0300

    Agrego mas tablas

--- data/script_creacion_inicial.sql
+++ data/script_creacion_inicial.sql
@@ -16,14 +16,31 @@ CREATE SCHEMA TRAEME_LA_COPA_MESSI AUTHORIZATION gdHotel2018
 
 GO
 
-CREATE TABLE TRAEME_LA_COPA_MESSI.Usuario(
+CREATE TABLE TRAEME_LA_COPA_MESSI.Direccion(
+IdDir int IDENTITY(1,1) PRIMARY KEY,
+Ciudad nvarchar(255) NOT NULL,
+Calle nvarchar(255) NOT NULL,
+NroCalle int NOT NULL,
+Piso int NULL,
+Departamento nvarchar(255) NULL,
+Localidad nvarchar(255) NOT NULL,
+Pais nvarchar(255) NOT NULL
+);
+
+CREATE TABLE TRAEME_LA_COPA_MESSI.TipoDoc(
+IdTipo int IDENTITY(1,1) PRIMARY KEY,
+Descripcion nvarchar(255) NOT NULL
+);
+
+CREATE TABLE TRAEME_LA_COPA_MESSI.Usuario( --Falta direccion
 Username int IDENTITY(1,1) PRIMARY KEY,
 Pass nvarchar(255)  NOT NULL,
+Direccion int FOREIGN KEY REFERENCES TRAEME_LA_COPA_MESSI.Direccion(IdDir),
 Nombre nvarchar(255) NOT NULL,
 Apellido nvarchar(255) NOT NULL,
-TipoDoc nvarchar(255) NOT NULL,
+TipoDoc int FOREIGN KEY REFERENCES TRAEME_LA_COPA_MESSI.TipoDoc(IdTipo),
 NroDocumento int NOT NULL,
-Email nvarchar(255) UNIQUE,
+Email nvarchar(255) UNIQUE NOT NULL,
 Telefono int NULL,
 FechaNacimiento datetime NULL,
 LogsFallidos int NOT NULL
@@ -40,4 +57,36 @@ Username int FOREIGN KEY REFERENCES TRAEME_LA_COPA_MESSI.Usuario(Username),
 IdRol int FOREIGN KEY REFERENCES TRAEME_LA_COPA_MESSI.Rol(IdRol),
 CONSTRAINT IdRolPorUsuario PRIMARY KEY(Username,IdRol)
 );
+
+CREATE TABLE TRAEME_LA_COPA_MESSI.RegimenEstadia(
+IdRegimenEstadia int IDENTITY(1,1) PRIMARY KEY,
+Descripcion nvarchar(255) NOT NULL,
+PrecioBase int NOT NULL,
+Estado int NOT NULL --No seberia ser int, luego lo cambio
+);
+
+CREATE TABLE TRAEME_LA_COPA_MESSI.Hotel(
+IdHotel int IDENTITY(1,1) PRIMARY KEY,
+Nombre nvarchar(255) NOT NULL,
+Mail nvarchar(255) UNIQUE NOT NULL,
+Telefono int NOT NULL,
+Direccion int FOREIGN KEY REFERENCES TRAEME_LA_COPA_MESSI.Direccion(IdDir),
+CantEstrellas int NOT NULL,
+PorcentajeEstrellas numeric(18,0) NOT NULL,
+FechaCreacion datetime NOT NULL
+);
+
+CREATE TABLE TRAEME_LA_COPA_MESSI.RegimenPorHotel(
+IdHotel int FOREIGN KEY REFERENCES TRAEME_LA_COPA_MESSI.Hotel(IdHotel),
+IdRegimenEstadia int FOREIGN KEY REFERENCES TRAEME_LA_COPA_MESSI.RegimenEstadia(IdRegimenEstadia)
+CONSTRAINT IdRegimenPorHotel PRIMARY KEY(IdHotel,IdRegimenEstadia)
+);
+
+CREATE TABLE TRAEME_LA_COPA_MESSI.InhabilitacionesHotel(
+IdHotel int FOREIGN KEY REFERENCES TRAEME_LA_COPA_MESSI.Hotel(IdHotel) PRIMARY KEY,
+FechaInicio datetime NOT NULL,
+FechaFin datetime NULL,
+Descripcion nvarchar(255) NOT NULL
+);
+
 
