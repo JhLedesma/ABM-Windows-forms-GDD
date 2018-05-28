07aa979ff7398810d6a44e2dc5c2e55609ce8bf2
kechum <jh.ledesmaa@gmail.com>
2018-05-28 14:57:09 -0300

    Refactor de Listado Cliente

---
+++ AbmCliente/List_Select_Cliente_Modificar.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FrbaHotel.AbmCliente
+{
+    public partial class List_Select_Cliente_Modificar : Form
+    {
+        public List_Select_Cliente_Modificar()
+        {
+            InitializeComponent();
+        }
+    }
+}
