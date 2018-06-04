26e35f6d67810272157f2c35daa8faceca9e5ae4
GonzaV <vieygonzalo@gmail.com>
2018-06-04 13:43:34 -0300

    Empezando filtrado de fechas para baja de hotel

---
+++ AbmHotel/Baja_hotel.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace FrbaHotel.AbmHotel
+{
+    partial class Baja_hotel
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.fecha_hasta = new System.Windows.Forms.DateTimePicker();
+            this.fecha_desde = new System.Windows.Forms.DateTimePicker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.boton_dar_baja = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.fecha_hasta);
+            this.groupBox1.Controls.Add(this.fecha_desde);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(269, 117);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Periodo de baja";
+            // 
+            // fecha_hasta
+            // 
+            this.fecha_hasta.Location = new System.Drawing.Point(54, 71);
+            this.fecha_hasta.Name = "fecha_hasta";
+            this.fecha_hasta.Size = new System.Drawing.Size(200, 20);
+            this.fecha_hasta.TabIndex = 3;
+            // 
+            // fecha_desde
+            // 
+            this.fecha_desde.Location = new System.Drawing.Point(54, 31);
+            this.fecha_desde.Name = "fecha_desde";
+            this.fecha_desde.Size = new System.Drawing.Size(200, 20);
+            this.fecha_desde.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Hasta:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Desde:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(22, 159);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(91, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Motivo de la baja:";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 188);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(269, 96);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // boton_dar_baja
+            // 
+            this.boton_dar_baja.Location = new System.Drawing.Point(206, 310);
+            this.boton_dar_baja.Name = "boton_dar_baja";
+            this.boton_dar_baja.Size = new System.Drawing.Size(75, 23);
+            this.boton_dar_baja.TabIndex = 4;
+            this.boton_dar_baja.Text = "Dar de baja";
+            this.boton_dar_baja.UseVisualStyleBackColor = true;
+            this.boton_dar_baja.Click += new System.EventHandler(this.boton_dar_baja_Click);
+            // 
+            // Baja_hotel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(295, 360);
+            this.Controls.Add(this.boton_dar_baja);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Baja_hotel";
+            this.Text = "Baja_hotel";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DateTimePicker fecha_hasta;
+        private System.Windows.Forms.DateTimePicker fecha_desde;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button boton_dar_baja;
+
+    }
+}
