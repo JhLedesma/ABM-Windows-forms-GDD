5af03060af1293aabd5379871605a515e9289d63
kechum <jh.ledesmaa@gmail.com>
2018-06-25 23:13:34 -0300

    Vsita de Creacion de Reserva Guest Completa

--- GenerarModificacionReserva/Generar_Reserva_Guest.Designer.cs
+++ GenerarModificacionReserva/Generar_Reserva_Guest.Designer.cs
@@ -34,17 +34,30 @@
             this.dtHasta = new System.Windows.Forms.DateTimePicker();
             this.lblRegimen = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.tbRegimen = new System.Windows.Forms.TextBox();
-            this.btnRegimen = new System.Windows.Forms.Button();
-            this.lblHotel = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.lblHabitacionPorDia = new System.Windows.Forms.Label();
-            this.btnModificar = new System.Windows.Forms.Button();
-            this.btnGuardar = new System.Windows.Forms.Button();
             this.lblCostoHabitacion = new System.Windows.Forms.Label();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.btnModificar = new System.Windows.Forms.Button();
+            this.lblHabitacionPorDia = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.lblHotel = new System.Windows.Forms.Label();
+            this.btnRegimen = new System.Windows.Forms.Button();
+            this.tbRegimen = new System.Windows.Forms.TextBox();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblCostoTotal = new System.Windows.Forms.Label();
+            this.btnConfirmar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.rbSi = new System.Windows.Forms.RadioButton();
+            this.rbNo = new System.Windows.Forms.RadioButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.btnRegistrar = new System.Windows.Forms.Button();
+            this.btnTerminar = new System.Windows.Forms.Button();
+            this.btnVolver = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
             this.SuspendLayout();
             // 
             // lblFechaDesde
@@ -103,74 +116,12 @@
             this.groupBox1.Controls.Add(this.lblHasta);
             this.groupBox1.Controls.Add(this.dtHasta);
             this.groupBox1.Controls.Add(this.dtDesde);
-            this.groupBox1.Location = new System.Drawing.Point(25, 12);
+            this.groupBox1.Location = new System.Drawing.Point(19, 12);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(305, 270);
             this.groupBox1.TabIndex = 5;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "PASO 1: Ingrese los siguientes datos para generar Reserva";
-            // 
-            // tbRegimen
-            // 
-            this.tbRegimen.Enabled = false;
-            this.tbRegimen.Location = new System.Drawing.Point(77, 148);
-            this.tbRegimen.Name = "tbRegimen";
-            this.tbRegimen.Size = new System.Drawing.Size(100, 20);
-            this.tbRegimen.TabIndex = 5;
-            // 
-            // btnRegimen
-            // 
-            this.btnRegimen.Location = new System.Drawing.Point(202, 146);
-            this.btnRegimen.Name = "btnRegimen";
-            this.btnRegimen.Size = new System.Drawing.Size(75, 23);
-            this.btnRegimen.TabIndex = 6;
-            this.btnRegimen.Text = "Seleccionar";
-            this.btnRegimen.UseVisualStyleBackColor = true;
-            // 
-            // lblHotel
-            // 
-            this.lblHotel.AutoSize = true;
-            this.lblHotel.Location = new System.Drawing.Point(6, 114);
-            this.lblHotel.Name = "lblHotel";
-            this.lblHotel.Size = new System.Drawing.Size(35, 13);
-            this.lblHotel.TabIndex = 7;
-            this.lblHotel.Text = "Hotel:";
-            // 
-            // comboBox1
-            // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(77, 109);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(125, 21);
-            this.comboBox1.TabIndex = 8;
-            // 
-            // lblHabitacionPorDia
-            // 
-            this.lblHabitacionPorDia.AutoSize = true;
-            this.lblHabitacionPorDia.Location = new System.Drawing.Point(6, 193);
-            this.lblHabitacionPorDia.Name = "lblHabitacionPorDia";
-            this.lblHabitacionPorDia.Size = new System.Drawing.Size(136, 13);
-            this.lblHabitacionPorDia.TabIndex = 9;
-            this.lblHabitacionPorDia.Text = "Valor de habitacion por dia:";
-            // 
-            // btnModificar
-            // 
-            this.btnModificar.Enabled = false;
-            this.btnModificar.Location = new System.Drawing.Point(35, 237);
-            this.btnModificar.Name = "btnModificar";
-            this.btnModificar.Size = new System.Drawing.Size(75, 23);
-            this.btnModificar.TabIndex = 10;
-            this.btnModificar.Text = "Modificar";
-            this.btnModificar.UseVisualStyleBackColor = true;
-            // 
-            // btnGuardar
-            // 
-            this.btnGuardar.Location = new System.Drawing.Point(162, 237);
-            this.btnGuardar.Name = "btnGuardar";
-            this.btnGuardar.Size = new System.Drawing.Size(75, 23);
-            this.btnGuardar.TabIndex = 11;
-            this.btnGuardar.Text = "Guardar";
-            this.btnGuardar.UseVisualStyleBackColor = true;
             // 
             // lblCostoHabitacion
             // 
@@ -182,29 +133,211 @@
             this.lblCostoHabitacion.TabIndex = 12;
             this.lblCostoHabitacion.Text = "0.00";
             // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(222, 237);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(75, 23);
+            this.btnGuardar.TabIndex = 11;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            // 
+            // btnModificar
+            // 
+            this.btnModificar.Enabled = false;
+            this.btnModificar.Location = new System.Drawing.Point(127, 237);
+            this.btnModificar.Name = "btnModificar";
+            this.btnModificar.Size = new System.Drawing.Size(75, 23);
+            this.btnModificar.TabIndex = 10;
+            this.btnModificar.Text = "Modificar";
+            this.btnModificar.UseVisualStyleBackColor = true;
+            // 
+            // lblHabitacionPorDia
+            // 
+            this.lblHabitacionPorDia.AutoSize = true;
+            this.lblHabitacionPorDia.Location = new System.Drawing.Point(6, 193);
+            this.lblHabitacionPorDia.Name = "lblHabitacionPorDia";
+            this.lblHabitacionPorDia.Size = new System.Drawing.Size(136, 13);
+            this.lblHabitacionPorDia.TabIndex = 9;
+            this.lblHabitacionPorDia.Text = "Valor de habitacion por dia:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(77, 109);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(125, 21);
+            this.comboBox1.TabIndex = 8;
+            // 
+            // lblHotel
+            // 
+            this.lblHotel.AutoSize = true;
+            this.lblHotel.Location = new System.Drawing.Point(6, 114);
+            this.lblHotel.Name = "lblHotel";
+            this.lblHotel.Size = new System.Drawing.Size(35, 13);
+            this.lblHotel.TabIndex = 7;
+            this.lblHotel.Text = "Hotel:";
+            // 
+            // btnRegimen
+            // 
+            this.btnRegimen.Location = new System.Drawing.Point(202, 146);
+            this.btnRegimen.Name = "btnRegimen";
+            this.btnRegimen.Size = new System.Drawing.Size(75, 23);
+            this.btnRegimen.TabIndex = 6;
+            this.btnRegimen.Text = "Seleccionar";
+            this.btnRegimen.UseVisualStyleBackColor = true;
+            // 
+            // tbRegimen
+            // 
+            this.tbRegimen.Enabled = false;
+            this.tbRegimen.Location = new System.Drawing.Point(77, 148);
+            this.tbRegimen.Name = "tbRegimen";
+            this.tbRegimen.Size = new System.Drawing.Size(100, 20);
+            this.tbRegimen.TabIndex = 5;
+            // 
             // groupBox2
             // 
-            this.groupBox2.Location = new System.Drawing.Point(25, 288);
+            this.groupBox2.Controls.Add(this.btnCancelar);
+            this.groupBox2.Controls.Add(this.btnConfirmar);
+            this.groupBox2.Controls.Add(this.lblCostoTotal);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Location = new System.Drawing.Point(19, 288);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(305, 113);
+            this.groupBox2.Size = new System.Drawing.Size(305, 127);
             this.groupBox2.TabIndex = 6;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "PASO 2: Confirme la Reserva";
             // 
             // groupBox3
             // 
-            this.groupBox3.Location = new System.Drawing.Point(25, 407);
+            this.groupBox3.Controls.Add(this.btnRegistrar);
+            this.groupBox3.Controls.Add(this.btnBuscar);
+            this.groupBox3.Controls.Add(this.label2);
+            this.groupBox3.Controls.Add(this.rbNo);
+            this.groupBox3.Controls.Add(this.rbSi);
+            this.groupBox3.Location = new System.Drawing.Point(19, 421);
             this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(305, 223);
+            this.groupBox3.Size = new System.Drawing.Size(305, 139);
             this.groupBox3.TabIndex = 7;
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "PASO 3: Ingrese datos de Cliente";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Costo total de reserva:";
+            // 
+            // lblCostoTotal
+            // 
+            this.lblCostoTotal.AutoSize = true;
+            this.lblCostoTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCostoTotal.Location = new System.Drawing.Point(198, 44);
+            this.lblCostoTotal.Name = "lblCostoTotal";
+            this.lblCostoTotal.Size = new System.Drawing.Size(45, 24);
+            this.lblCostoTotal.TabIndex = 13;
+            this.lblCostoTotal.Text = "0.00";
+            // 
+            // btnConfirmar
+            // 
+            this.btnConfirmar.Location = new System.Drawing.Point(222, 94);
+            this.btnConfirmar.Name = "btnConfirmar";
+            this.btnConfirmar.Size = new System.Drawing.Size(75, 23);
+            this.btnConfirmar.TabIndex = 13;
+            this.btnConfirmar.Text = "Confirmar";
+            this.btnConfirmar.UseVisualStyleBackColor = true;
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Enabled = false;
+            this.btnCancelar.Location = new System.Drawing.Point(127, 94);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(75, 23);
+            this.btnCancelar.TabIndex = 14;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            // 
+            // rbSi
+            // 
+            this.rbSi.AutoSize = true;
+            this.rbSi.Location = new System.Drawing.Point(184, 51);
+            this.rbSi.Name = "rbSi";
+            this.rbSi.Size = new System.Drawing.Size(34, 17);
+            this.rbSi.TabIndex = 0;
+            this.rbSi.TabStop = true;
+            this.rbSi.Text = "Si";
+            this.rbSi.UseVisualStyleBackColor = true;
+            // 
+            // rbNo
+            // 
+            this.rbNo.AutoSize = true;
+            this.rbNo.Location = new System.Drawing.Point(238, 51);
+            this.rbNo.Name = "rbNo";
+            this.rbNo.Size = new System.Drawing.Size(39, 17);
+            this.rbNo.TabIndex = 1;
+            this.rbNo.TabStop = true;
+            this.rbNo.Text = "No";
+            this.rbNo.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(129, 13);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "¿Ya es cliente registrado?";
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Enabled = false;
+            this.btnBuscar.Location = new System.Drawing.Point(127, 107);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(75, 23);
+            this.btnBuscar.TabIndex = 15;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            // 
+            // btnRegistrar
+            // 
+            this.btnRegistrar.Enabled = false;
+            this.btnRegistrar.Location = new System.Drawing.Point(222, 107);
+            this.btnRegistrar.Name = "btnRegistrar";
+            this.btnRegistrar.Size = new System.Drawing.Size(75, 23);
+            this.btnRegistrar.TabIndex = 16;
+            this.btnRegistrar.Text = "Registrar";
+            this.btnRegistrar.UseVisualStyleBackColor = true;
+            // 
+            // btnTerminar
+            // 
+            this.btnTerminar.Enabled = false;
+            this.btnTerminar.Location = new System.Drawing.Point(241, 576);
+            this.btnTerminar.Name = "btnTerminar";
+            this.btnTerminar.Size = new System.Drawing.Size(75, 23);
+            this.btnTerminar.TabIndex = 17;
+            this.btnTerminar.Text = "Terminar";
+            this.btnTerminar.UseVisualStyleBackColor = true;
+            // 
+            // btnVolver
+            // 
+            this.btnVolver.Enabled = false;
+            this.btnVolver.Location = new System.Drawing.Point(28, 576);
+            this.btnVolver.Name = "btnVolver";
+            this.btnVolver.Size = new System.Drawing.Size(75, 23);
+            this.btnVolver.TabIndex = 18;
+            this.btnVolver.Text = "Volver";
+            this.btnVolver.UseVisualStyleBackColor = true;
             // 
             // Generar_Reserva_Guest
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(354, 660);
+            this.ClientSize = new System.Drawing.Size(344, 611);
+            this.Controls.Add(this.btnVolver);
+            this.Controls.Add(this.btnTerminar);
             this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
@@ -212,6 +345,10 @@
             this.Text = "Generar Reserva";
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -234,5 +371,16 @@
         private System.Windows.Forms.TextBox tbRegimen;
         private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button btnConfirmar;
+        private System.Windows.Forms.Label lblCostoTotal;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnRegistrar;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton rbNo;
+        private System.Windows.Forms.RadioButton rbSi;
+        private System.Windows.Forms.Button btnTerminar;
+        private System.Windows.Forms.Button btnVolver;
     }
 }
